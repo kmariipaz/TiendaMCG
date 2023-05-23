c8aecec9316af71c711692c00f9caeb3a589bf83
Maripaz <Maripaz@LAPTOP-FPG4SHTF>
2023-05-23 10:11:55 -0600

    Creacion de Proyecto

---
+++ src/main/java/com/TiendaMCG/TiendaMcgApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaMCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMcgApplication.class, args);
+	}
+
+}
